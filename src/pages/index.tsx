6cef38e5e49513c2c9eb73b6bf5a4ecfee8a5f61
Jalen Parham <jalenparham97@gmail.com>
2022-11-30 20:13:16 -0500

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold">Jalen Parham</h1>
+    </div>
+  );
+}
