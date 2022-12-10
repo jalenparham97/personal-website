cde23d35ad6e74bd3b9a3aec4271f638d9d21b2b
Jalen Parham <jalenparham97@gmail.com>
2022-12-09 19:05:10 -0500

    Removing unused imports from app.ts file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from '@next/font/google';
-import { Navbar } from '@/components/ui/Navbar';
-import { Footer } from '@/components/ui/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
